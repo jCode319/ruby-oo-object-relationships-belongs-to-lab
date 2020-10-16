5ad2e1d7d2d69df496ffdeede4d9caf6224e12dc
jCode319 <jdjack3@gmail.com>
2020-10-16 09:06:01 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,12 @@
+class Post
+
+    attr_accessor :title, :author,
+
+    def initialize(author, title)
+        @author = author
+        @title = title
+    end
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,10 @@
+class Song
+
+    attr_accessor :title, :artist,
+
+    def initialize(artist, title)
+        @title = title
+        @artist = artist
+    end
+
+end
